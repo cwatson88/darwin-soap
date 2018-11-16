0425d1c235687100fb9ff5ce85a08400b1ca195c
Chirs Watson <cwatson1988@gmail.com>
2018-11-16 09:02:02 +0000

    Remove the JSON method and WDSL option in the function call.

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
+
+
+/*
+### GetNextDepartures:
+
+see https: //lite.realtime.nationalrail.co.uk/OpenLDBsvWS/
+for full SOAP info
+
+*/
+
 var soap = require("soap");
 var url = "https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/wsdl.aspx";
 
-// this worked and put sv: infront of *MOST of the xml, see args below
-const wsdlOptions = {
-  overrideRootElement: {
-    namespace: 'sv:',
-    xmlnsAttributes: [{
-      name: 'sv',
-      value: "http://thalesgroup.com/RTTI/2017-10-01/ldbsv/"
-    }]
-  }
-};
 
 // Headers needed the colon to remove the :sv due to the wsdlOptions implimentation
 const headers = {
   ":AccessToken": {
     ":TokenValue": "e225d22d-7be9-45cc-b9f8-0398271f5fe3"
-  }
-};
-
-/* 
-using args didn't work as the filter list didn't include the pre - tag sv:
-Instead now use the template strings below (req) this works
-*/
-const args = {
-  "sv:GetNextDeparturesRequest":{
-    "sv:crs": "EUS",
-    "sv:filterList": [{
-      "sv:crs": "BHI"
-    }],
-    "sv:time": "2018-11-11T20:00:00",
-    "sv:timeWindow": 120,
-    "sv:filterTOC": null,
-    "sv:services": "P"
   }
 };
 
@@ -54,7 +37,7 @@ const args2 = {
 }
 
 
-soap.createClient(url, wsdlOptions, function (err, client) {
+soap.createClient(url, function (err, client) {
   client.addSoapHeader(headers);
   // console.log(client)
   client.GetNextDeparturesAsync(args2).then((result) => {
@@ -64,11 +47,3 @@ soap.createClient(url, wsdlOptions, function (err, client) {
   )
 });
 
-/* 
-crs (string, 3 characters, alphabetic): The CRS code of a requested station origin.
-filterList (string[], 3 characters, alphabetic): A collection of CRS codes that are the destinations from the above CRS.
-time (DateTime): Time of departure board.
-timeWindow (integer between 0 and 1440): How far into the future in minutes, relative to the time, to return services for. Optional.
-filterTOC (string): The TOC code to apply to the filter. Filters services to include only those belonging to the specified TOC. Optional.
-services (string) : P for train Services, B for bus services, S for ship services. Defaults to "P". Optional.
-*/
