eda622505b1c673f6bee596819994c8407c3305b
Chris Watson <cwatson1988@gmail.com>
2018-11-17 22:52:29 +0000

    changed the calls to DARWIN to async. Also added routes. Currently a test file for routes for Herouku.

--- getNextDepartures.js
+++ getNextDepartures.js
@@ -13,7 +13,7 @@ const {
 
 const departureStation = 'EUS';
 const destinationStation = 'BHI';
-const dateTime = new Date('2018/11/16 10:30').toJSON();
+const dateTime = new Date('2018/11/17 20:30').toJSON();
 const minutesTo = 180; // integer between 0 and 1440 mins
 
 const req = `
@@ -28,13 +28,16 @@ const req = `
         <sv:services>P</sv:services>
     </sv:GetNextDeparturesRequest>`;
 
-const args2 = {
+const args = {
   _xml: JSON.stringify(req).replace(/\\n|\\t/g, ''),
 };
 
-soap.createClient(url, async (err, client) => {
-  client.addSoapHeader(headers);
-  const result = await client.GetNextDeparturesAsync(args2);
+const getNextDepartures = async () => {
+  const client = await soap.createClientAsync(url);
+  await client.addSoapHeader(headers);
+  const result = await client.GetNextDeparturesAsync(args);
   const train = await result[0].DeparturesBoard.departures;
   return train;
-});
+};
+
+module.exports = { getNextDepartures };
--- index.js
+++ index.js
@@ -1,61 +1,26 @@
 const express = require('express');
-
-
-/*
-### GetNextDepartures:
-
-see https: //lite.realtime.nationalrail.co.uk/OpenLDBsvWS/
-for full SOAP info
-
-*/
-const {
-  soap,
-  url,
-  headers,
-} = require('./darwin-config.js');
-
-const departureStation = 'EUS';
-const destinationStation = 'BHI';
-const dateTime = new Date().toJSON();
-const minutesTo = 180; // integer between 0 and 1440 mins
-
-const reqData = `
-    <sv:GetNextDeparturesRequest>
-        <sv:crs>${departureStation}</sv:crs>
-        <sv:filterList>
-            <sv:crs>${destinationStation}</sv:crs>
-        </sv:filterList>
-        <sv:time>${dateTime}</sv:time>
-        <sv:timeWindow>${minutesTo}</sv:timeWindow>
-        <sv:filterTOC></sv:filterTOC>
-        <sv:services>P</sv:services>
-    </sv:GetNextDeparturesRequest>`;
-
-const args = {
-  _xml: JSON.stringify(reqData).replace(/\\n|\\t/g, ''),
-};
+const { getNextDepartures } = require('./getNextDepartures.js');
+const test = require('./Routes/test');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const requestTime = function (req, res, next) {
-  req.requestTime = Date.now();
+  req.requestTime = new Date(Date.now()).toUTCString();
   next();
 };
 
 app.use(requestTime);
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    soap.createClient(url, async (err, client) => {
-      await client.addSoapHeader(headers);
-      const result = await client.GetNextDeparturesAsync(args);
-      const train = await result[0].DeparturesBoard.departures;
-      res.send({ message: { time: req.requestTime, train } });
-    });
+    const trains = await getNextDepartures();
+    res.send({ message: { time: req.requestTime, trains } });
   } catch (error) {
     console.log(error);
   }
 });
+
+app.use('/test', test);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
